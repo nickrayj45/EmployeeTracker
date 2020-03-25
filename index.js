3160f474b662cd7914b1d886b7491f5a7b69a5c8
Nicholas Jaquez <nrjaquez@gmail.com>
2020-03-24 23:46:42 -0700

    commit 2

--- index.js
+++ index.js
@@ -2,11 +2,72 @@
 const inquirer = require("inquirer");
 const holdInfo = [];
 
+var mysql = require("mysql");
+var consoleTable = require("console.table")
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "work_db"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  runSearch();
+});
+
+
+
+
+
 function userQuestions() {
   inquirer.prompt({
     name: "action",
     type: "list",
     message: "What would you like to do?",
-    choices: []
-  });
+    choices: [
+      // Questions
+      "Add a department?",
+      "Add a role?",
+      "Add an employee?",
+      "View a depratment",
+      "View a role?",
+      "View an employee?",
+      "Update an employee?"
+    ]
+  })
+  .then( function (answer){
+    console.log(holdInfo)
+    switch (answer.action){
+      case "Add a department?":
+        createDepartment();
+        break
+      case "Add a role?":
+        addRole();
+        break
+      case "Add an employee?":
+        addEmployee();
+        break
+        case "View a department?":
+          viewDepartment();
+        break  
+        case "View a role?":
+        viewRole();
+        break  
+        case "view an employee?":
+        viewEmployee();
+        break
+        case "Update an employee?":
+        updateEmployee();
+        break
+    }
+  })
 }
+

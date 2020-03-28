3732fdc2377f1d796ed1030e6b283d74898a528e
Nicholas Jaquez <nrjaquez@gmail.com>
2020-03-28 00:00:11 -0700

    functions improving

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ function userQuestions() {
         "Add a department?",
         "Add a role?",
         "Add an employee?",
-        "View a depratment",
+        "View a department?",
         "View a role?",
         "View an employee?",
         "Update an employee's role??",
@@ -67,104 +67,149 @@ function userQuestions() {
         case "Update an employee?":
           updateEmployee();
           break;
-        case "Complete?"  :
-        connection.end();
-        process.exit();
+        case "Complete?":
+          connection.end();
+          process.exit();
       }
     });
 }
 
 function createDepartment() {
-  inquirer.prompt({
+  inquirer
+    .prompt({
       name: "newDept",
       type: "input",
       message: "What is the name of the department would you like to add?"
-  })   
-  .then(function(answer) {
+    })
+    .then(function(answer) {
       //no need for user to view this
       //var query = "SELECT name FROM department";
-      connection.query("INSERT INTO department SET ?",
-      {
+      connection.query(
+        "INSERT INTO department SET ?",
+        {
           name: answer.newDept
-      },
-      function(err, res) {
+        },
+        function(err, res) {
           if (err) throw err;
-          console.log(res)
-          console.table(res)
-          console.log("You created a new department")
-
-      })
-
-      userQuestions()
-
-  })
+          // console.log(res);
+          console.table(res);
+          console.log("You created a new department");
+          userQuestions();
+        }
+      );
+    });
 }
 
 function createRole() {
-  inquirer.prompt([
-    {
-      name: "newRole",
-      type: "input",
-      message: "What role would you like to add?"
-    },
-    {
-      name: "addSalary",
-      type: "input",
-      message: "What is the salary of this role?"
-    },
-    {
-      name: "deptId",
-      type: "input",
-      message: "What is the department ID?"
-    }])   
-  .then(function(answer) {
+  inquirer
+    .prompt([
+      {
+        name: "newRole",
+        type: "input",
+        message: "What role would you like to add?"
+      },
+      {
+        name: "addSalary",
+        type: "input",
+        message: "What is the salary of this role?"
+      },
+      {
+        name: "deptId",
+        type: "input",
+        message: "What is the department ID?"
+      }
+    ])
+    .then(function(answer) {
       //no need for user to view this
       //var query = "SELECT name FROM department";
-      connection.query("SELECT id FROM department INNER JOIN role ON department_id?",
-      {
+      // SELECT id FROM department INNER JOIN role ON department_id?
+      connection.query(
+        "INSERT INTO role SET ?",
+        {
           title: answer.newRole,
           salary: answer.addSalary,
           department_id: answer.deptId
-      },
-      function(err, res) {
-          if (err) throw err;
-          console.log("You created a new role")
-      })
-  })
-}
-//   function addEmployee() {
-//     inquirer.prompt([
-//       {
-//         name: "first_name",
-//         type: "input",
-//         message: "What is the employee's first name?"
-//       },
-//       {
-//         name: "last_name",
-//         type: "input",
-//         message: "What is the employee's last name?"
-//       },
-//       {
-//         name: "role",
-//         type: "input",
-//         message: "What is the employee's role?"
-//       }])   
-//     .then(function(answer) {
-//         //no need for user to view this
-//         //var query = "SELECT name FROM department";
-//         connection.query("INSERT INTO department SET ?",
-//         {
-//             first_name: answer.first_name,
-//             last_name: answer.last_name,
-//             role: answer.role
-//         },
-//         function(err, res) {
-//             if (err) throw err;
-//             console.log("You created a new employee")
-//         })
-//     })
-  
+        },
 
-// }
+        function(err, res) {
+          if (err) throw err;
+          console.log("You created a new role");
+          userQuestions();
+        }
+      );
+    });
+}
+function addEmployee() {
+  connection.query("SELECT * FROM role", function(err, res) {
+    var roleChoices = res.map(({id, title}) => ({value:id, name:title}))   
+    connection.query("SELECT first_name, last_name, employee.id FROM employee LEFT JOIN role ON employee.role_id = role.id WHERE role.title = 'Manager'",    
+    function(err, res) {        
+      var managerList = res.map(({first_name, last_name, id}) => ({value:id, name:`${first_name} ${last_name}`}))
+    inquirer
+      .prompt([
+        {
+          name: "first_name",
+          type: "input",
+          message: "What is the employee's first name?"
+        },
+        {
+          name: "last_name",
+          type: "input",
+          message: "What is the employee's last name?"
+        },
+        {
+          name: "role",
+          type: "input",
+          message: "What is the employee's role?",
+          choices: roleChoices
+        }
+        
+      ])
+      .then(function(answer) {
+        //no need for user to view this
+        //var query = "SELECT name FROM department";
+        connection.query(
+          "INSERT INTO employee SET ?",
+          {
+            first_name: answer.first_name,
+            last_name: answer.last_name,
+            role_id: answer.role
+          },
+          function (err) {
+            if (err) throw err;
+            console.log("You created a new employee");
+            userQuestions();
+          }
+        );
+      });
+  });
+}
 
 // View functions should be select queries console.log(response from queries)
+
+function viewDepartment() {
+  connection.query("SELECT * FROM department", function(err, res) {
+    if (err) throw err;
+    console.table(res);
+    userQuestions();
+
+  });
+}
+
+function viewRole() {
+  connection.query("SELECT * FROM role", function(err, res) {
+    if (err) throw err;
+    console.table(res);
+    userQuestions();
+
+  });
+}
+
+function viewEmployee() {
+  connection.query("SELECT * FROM employee", function(err, res) {
+    if (err) throw err;
+    console.table(res);
+    userQuestions();
+
+  });
+}
